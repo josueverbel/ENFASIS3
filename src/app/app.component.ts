a4601c0efad8416a9b06bef4d34756cb0c4926f4
Josue verbel <josueverbel@gmail.com>
2022-03-09 19:50:45 -0500

    Listando arrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from './Models/Country.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'Proyecto de prueba';
+
+  public myvarString : string  = "";
+  public myVarNumber : Number = 1;
+  public myVarBool : boolean = false;
+
+
+  constructor () {
+    
+    
+    
+  }
+
+
 }
+
+
--- src/app/theme/main/main.component.ts
+++ src/app/theme/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from 'src/app/Models/Country.model';
 
 @Component({
   selector: 'app-main',
@@ -6,8 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  public countries : Array<Country> = [];
 
-  constructor() { }
+  constructor() {
+    let country1 : Country = {
+      Id: 1, 
+      Name : "Colombia", 
+      Description : "Colombia"
+    };
+    let country2 : Country = {
+      Id: 2, 
+      Name : "Argentina", 
+      Description : "Argentina"
+    };
+    let country3 : Country = {
+      Id: 3, 
+      Name : "Canada", 
+      Description : "Canada"
+    };
+    
+    this.countries.push(country1);
+    this.countries.push(country2);
+    this.countries.push(country3);
+
+    let mayores = this.countries.filter(f => f.Id > 1);
+    let pais3 = this.countries.find(f => f.Id == 3);
+    let c : Country = { Id: 3, 
+      Name : "Canada", 
+      Description : "Canada"};
+    if (pais3) {
+      let index = this.countries.indexOf(pais3);
+      this.countries.splice(index, 1);
+
+    }
+    this.countries.push(c);
+
+    
+  
+    console.log(this.countries);
+    console.log(mayores);
+    console.log(pais3);
+    
+   }
 
   ngOnInit(): void {
   }
